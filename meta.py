4f87679d86d82dd48003a0d8532a02f308664d45
M. H. Shakiba <97367796+abikahs@users.noreply.github.com>
2023-08-20 03:07:24 +0330

    Add files via upload

---
+++ meta.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import os, sys
+
+while True:
+    if len(sys.argv) != 3:
+        print("Usage: python3 meta.py <PATH> <CSV Name>")
+        sys.exit(1)
+    else:
+        break
+
+path = sys.argv[1]
+csv = sys.argv[2]
+folder = sys.argv[2]
+try:
+    df = pd.read_csv(f"{path}/{csv}.csv")
+    ndf = df[['Title', 'FirstAuthor', 'DOI','PublicationYear']]
+
+except:
+    sys.exit("CSV EROR!!")
+
+
+folder_path = f"{path}/{folder}"
+
+# Check if the folder already exists before creating it
+if not os.path.exists(folder_path):
+    try:
+        # Create the new folder
+        os.mkdir(folder_path)
+    except OSError as e:
+        sys.exit(f"Error: {e}")
+else:
+    sys.exit("Folder exists!")
+
+id = 1
+for index, row in ndf.iterrows():
+    if pd.isnull(row['DOI']):
+        continue
+
+    file_path = f"{path}/{folder}/{id}.txt"
+    with open(file_path, "w") as file:
+        try:
+            file.write(f"-> {int(row['PublicationYear'])} | {csv}\n\nTitle: {row['Title']}\n\nAuthor(s): {row['FirstAuthor']}\n\n˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙\n{row['DOI']}")
+        except:
+            file.write(f"-> {row['PublicationYear']} | {csv}\n\nTitle: {row['Title']}\n\nAuthor(s): {row['FirstAuthor']}\n\n˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙˙\n{row['DOI']}")
+    id += 1
+
+print("Done! ∂")
---
+++ zotero.py
@@ -0,0 +1,44 @@
+import requests
+import sys
+def fetch_and_write(doi, output_file):
+    try:
+        response = requests.get(f"https://doi.org/{doi}", headers={"Accept": "text/bibliography; style=bibtex"})
+        if response.status_code == 200:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write(response.text + "\n")
+                print(f"DOI {doi} fetched and written to {output_file}")
+        else:
+            print(f"Failed to fetch DOI {doi}. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error fetching DOI {doi}: {e}")
+
+def process_doi_file(input_file, output_file):
+    with open(input_file, "r", encoding="utf-8") as file:
+        dois = file.readlines()
+        
+    for doi in dois:
+        doi = doi.strip()  # Remove leading/trailing whitespace
+        fetch_and_write(doi, output_file)
+
+def main():
+    while True:
+        if len(sys.argv) != 3:
+            print("Usage: python zotero.py <PATH> <.txt Name>")
+            sys.exit(1)
+        else:
+            break
+    
+    path = sys.argv[1]
+    txt = sys.argv[2]
+    bib = sys.argv[2]
+    input_file = f"{path}/{txt}.txt"
+    output_file = f"{path}/{bib}.bib"
+    
+    process_doi_file(input_file, output_file)
+    
+    print("Done! ∂")
+
+if __name__ == "__main__":
+    main()
+
+

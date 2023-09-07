0460ab9aeb11cf7f4098410247d616de5ae38433
M. Shakiba <e.shakiba@icloud.com>
2023-09-07 16:48:31 +0330

    Update meta.py

--- main/meta.py
+++ main/meta.py
@@ -35,8 +35,7 @@ for id, (index, row) in enumerate(ndf.iterrows(), start=1):
 
     file_path = os.path.join(folder_path, f"{id}.txt")
     with open(file_path, "w") as file:
-        file.write(f"-> {str(row['PublicationYear'])[:4]} | {csv}\n\nTitle: {row['Title']}\n\nAuthors: {row['FirstAuthor']}\n\n–––––––––––––––––––––––––––––––––\n{row['DOI']}")
+        file.write(f"-> {str(row['PublicationYear'])[:4]} | {csv[:-4]}\n\nTitle: {row['Title']}\n\nAuthors: {row['FirstAuthor']}\n\n{row['DOI']}")
         file_list.append(["->", row['DOI']])
-
 print(tabulate(file_list, headers=["Found", "DOI"], tablefmt="github"))
 print(f"\ncheck -> {folder_path}")
